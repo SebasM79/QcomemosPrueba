c9b33829f5c5f5aa7c13c1c6e56bc136344fb28a
SebasM79 <sebamussi.dev@hotmail.com>
2024-09-20 18:19:59 -0300

    modificacion server

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+// fix para _dirname
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//servidor
+const app = express();// crea una instancia de express
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log("Servidor escuchando en el puerto ", app.get("port"));
+
+//configuracion de archivos estaticos
+app.use(express.static(__dirname + "/public"));
+
+//rutas
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/front/home.html");
+});
+
+app.get("/front", (req, res) => {
+    res.sendFile(__dirname + "/front/login.html");
+});
